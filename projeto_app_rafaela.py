4ebbb92bce8e5b6b9fd3abfb0cefb6d17d428a14
rafaelamatos2010s-a11y <rafaelamatos2010s@gmail.com>
2026-03-03 16:39:42 -0300

    update projeto rafaela
    Projeto salao de beleza, codigos

---
+++ projeto_app_rafaela.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+print('sistema de agendamento de aulas - Meet')
+
+nome_usuario = input('digite seu nome para continuar')
+
+aluno_turma = input('digite sua turma:('a',b',c',d'): ')
+
+email = input('digite seu e- mail') ('Gabriele.Alane@gamil.com':)
+
+if acesso_menu == '1':
+        print("Agendando aula...")
+
+        nome_aluno = input("Digite o nome do aluno: ")
+
+        turma_aluno = input("Digite a turma do aluno: ")
+
+        email_aluno = input("Digite o e-mail do aluno: ")
+        # Código para agendar aula
+    
+    elif acesso_menu == '0':
+        print("Saindo do sistema. Até logo!")
+        break
+
+#exemplo de indentação correta para o código acima:
+
+    else:
+        print("Opção inválida. Por favor, tente novamente.")
+
